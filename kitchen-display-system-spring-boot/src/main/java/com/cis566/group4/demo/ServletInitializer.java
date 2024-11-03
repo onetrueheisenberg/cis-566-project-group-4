edd32fe63699eff121f879cf09d3c3c7e1c477c7
Sundara Subramanian Swaminathan <sundarasubramanian.s@gmail.com>
2024-11-03 17:33:50 -0500

    testing some configuration changes

--- kitchen-display-system-spring-boot/src/main/java/com/cis566/group4/demo/ServletInitializer.java
+++ kitchen-display-system-spring-boot/src/main/java/com/cis566/group4/demo/ServletInitializer.java
@@ -3,11 +3,13 @@ package com.cis566.group4.demo;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import com.cis566.group4.demo.messaging.MessagingRabbitmqApplication;
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(DemoApplication.class);
+		return application.sources(MessagingRabbitmqApplication.class);
 	}
 
 }
