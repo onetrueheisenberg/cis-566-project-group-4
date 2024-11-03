acb4462eefb84d0820f4a0b1628d4f03eaf0ea5f
Sundara Subramanian Swaminathan <sundarasubramanian.s@gmail.com>
2024-11-03 18:14:31 -0500

    setting sending and receiving

---
+++ kitchen-display-system-spring-boot/src/main/java/com/cis566/group4/demo/messaging/RabbitMQReceiver.java
@@ -0,0 +1,18 @@
+package com.cis566.group4.demo.messaging;
+
+import com.cis566.group4.demo.messaging.model.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class RabbitMQReceiver {
+    private AtomicLong count = new AtomicLong(0L);
+
+    @RabbitListener(queues = "${sample.rabbitmq.queue}")
+    public void recievedMessage(Message message) {
+        System.out.println("( " + count.incrementAndGet() + " ) Received = : " + message);
+
+    }
+}
